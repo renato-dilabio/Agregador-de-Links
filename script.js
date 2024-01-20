89b65a59b9f8084a4d540940d442c5e29da3e2b2
renato-dilabio <renatodilabio@gmail.com>
2024-01-20 15:58:38 -0300

    background change based on media queries

--- script.js
+++ script.js
@@ -15,12 +15,12 @@ function toggleMode() {
       "alt",
       "Foto de Renato sorrindo com feição pensativa e mão no queixo. Ele usa óculos, barba e seu cabelo cacheado está cortado baixo nas laterais e um pouco mais alto em cima."
     )
-    img.setAttribute("src", "./assets/assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
     img.setAttribute(
       "alt",
       "Foto de Renato sorrindo. Ele usa óculos escuros, barba e seu cabelo cacheado está cortado baixo nas laterais e um pouco mais alto em cima."
     )
-    img.setAttribute("src", "./assets/assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar.png")
   }
 }
